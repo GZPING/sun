3395e58af499e38e293cdf6b34ce98d912a72b61
GD <455607047@qq.com>
2019-08-22 22:54:09 +0800

    java 8

---
+++ baseJdk/src/main/java/java8/OptionalS.java
@@ -0,0 +1,60 @@
+package java8;
+
+/**
+ * Created by GD on 2019/8/22.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+/**
+ *
+ * @Description Opertion
+ * @Author GD
+ * @Date 2019/8/22 22:02
+ * @Version 1.0V
+ */
+
+public class OptionalS {
+
+    @Test
+    public void test(){
+        Integer val = null;
+        // ofNullable  设置值，orElse 如果为空，则选else 值
+        val = Optional.ofNullable(val).orElse(10);
+        System.out.println("value 1 : " + val);
+    }
+
+    @Test
+    public void test1(){
+        Integer val = null;
+        // ofNullable  设置值，orElse 如果为空，则选else 值
+        val = Optional.ofNullable(val).orElse(10);
+        System.out.println("value 1 : " + val);
+    }
+
+    @Test
+    public void test12(){
+        Optional<Integer> optional = Optional.empty();
+        optional.orElse(10);
+        Integer val = optional.get();
+        System.out.println("value 1 : " + val);
+    }
+
+
+    public static void main(String[] args) {
+        Integer val;
+        // of 设置Optional value 值，如果of 传入为null ，则报错
+        Optional<Integer> optional = Optional.of(10);
+        // get  获取value 值
+        try {
+            optional = Optional.of(null);
+        } catch (Exception e) {
+            System.out.println("ex");
+        }
+        val = optional.get();
+        System.out.println("value 1 : " + val);
+
+    }
+}
---
+++ baseJdk/src/main/java/java8/StreamS.java
@@ -0,0 +1,40 @@
+package java8;
+
+/**
+ * Created by GD on 2019/8/22.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @Description 流编程
+ * @Author GD
+ * @Date 2019/8/22 22:02
+ * @Version 1.0V
+ */
+
+public class StreamS {
+
+    @Test
+    public void test(){
+        List<Integer> ints = new ArrayList<>();
+        ints.add(10);ints.add(20);ints.add(1);ints.add(12);
+        ints.add(13);ints.add(5);ints.add(3);ints.add(2);
+        ints.forEach(s -> System.out.print(s + ","));
+        Collections.sort(ints,(s1,s2) -> s1.compareTo(s2));
+        System.out.println();
+        ints.forEach(s -> System.out.print(s + ","));
+        List<Integer> ints2 = ints.stream().filter(s -> s > 10 ).collect(Collectors.toList());
+        System.out.println();
+        ints2.forEach(s -> System.out.print(s + ","));
+        //ints;
+    }
+}
