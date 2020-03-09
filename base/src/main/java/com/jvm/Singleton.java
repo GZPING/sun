59278cadd035be2ea8c25ca5b253f24b9215cf2b
GZPING <455607047@qq.com>
2020-03-09 22:40:47 +0800

    Singleton 类加载过程

--- base/src/main/java/com/jvm/Singleton.java
+++ base/src/main/java/com/jvm/Singleton.java
@@ -3,6 +3,11 @@ package com.jvm;
 /**
  *
  * 类加载过程
+ * 1. main 方法中调用Singleton 的静态方法，此时Singleton 对象初始化
+ * 2. 对象初始化，将初始化变量x,y, y 默认值为0,
+ * 3. 调用new Singleton 构造函数，执行x++,y++ 操作，
+ * 4. 返回对象实例
+ *
  * @author : GD
  * @date :2020/3/9 : 22:26
  */
